a0aafb78cb356a04b2f719651d98492c92f338cd
Mihail <miha_gord@mail.ru>
2025-03-12 12:42:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 # Part of case-study #2
 # Case has been done by Mihail Gordeev and Sergey Chirkov
 # Collatz conjecture
+# Idea was taking from this video: https://www.youtube.com/watch?v=QgzBDZwanWA&ab_channel=VertDider
 
 
-'''main module for graphical display of calculations'''
+#main module for graphical display of calculations
 import matplotlib.pyplot as plt
 
 
-'''module localization for Russian language'''
+#module localization for Russian language
 import ru_local as ru
 
 
-'''def for graphical display'''
-'''y_ax - list of coordinates of the sequence (3n+1)'''
 def graf(y_ax):
-    '''setting up the display field'''
+    '''def for graphical display
+       y_ax - list of coordinates of the sequence (3n+1)'''
+
+    #setting up the display field
     plt.figure(figsize=(8, 5), dpi=100)
 
-    '''setting up the graph line'''
+    #setting up the graph line
     plt.plot(range(1, len(y_ax)+1), y_ax, color='red')
 
-    '''setting the labels of the abscissa and ordinate axes'''
+    #setting the labels of the abscissa and ordinate axes
     plt.xlabel(ru.Q_NUMBERS)
     plt.ylabel(ru.V_NUMBERS)
 
-    '''setting the title of the function graph'''
+    #setting the title of the function graph
     plt.title(ru.GRAPH_INTER, fontdict={'fontname': 'Arial', 'fontsize': 20})
 
-    '''function graph display'''
+    #function graph display
     plt.show()
 
 
-'''def for calculation and graphical display of the sequence (3n+1) or the Collatz hypothesis'''
+
 def main():
+    '''def for calculation and graphical display of the sequence (3n+1) or the Collatz hypothesis'''
+
     print(ru.COLLATZ_GREETINGS, ru.INTRODUCTION_COLLATZ, sep='\n')
     number = int(input(ru.ENTER_NUMBER))
     inter_res = [number]
@@ -50,6 +54,6 @@ def main():
     print(inter_res)
     graf(inter_res)
 
-'''checking if startup file is the main program'''
+
 if __name__ == '__main__':
     main()
